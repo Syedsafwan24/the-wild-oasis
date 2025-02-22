5071bdd838d79a455ba7021d5559e3ed89b7ca9f
Syedsafwan24 <136835578+Syedsafwan24@users.noreply.github.com>
2025-02-22 22:48:07 +0530

    added stateTransitions

--- app/_lib/action.js
+++ app/_lib/action.js
@@ -3,7 +3,9 @@
 import { revalidatePath } from 'next/cache';
 import { auth, signIn, signOut } from './auth';
 import { supabase } from './supabase';
+import { getBookings } from './data-service';
 
+// Update the guest's profile
 export async function updateProfile(formData) {
 	const session = await auth();
 	if (!session) throw new Error('You must be signed in to update your profile');
@@ -11,7 +13,9 @@ export async function updateProfile(formData) {
 	// Extract and validate national ID
 	const nationalID = formData.get('nationalID');
 	if (!nationalID || !/^[a-zA-Z0-9]{6,12}$/.test(nationalID)) {
-		return { error: 'National ID must be between 6 and 12 alphanumeric characters' };
+		return {
+			error: 'National ID must be between 6 and 12 alphanumeric characters',
+		};
 	}
 
 	// Extract and validate nationality & countryFlag
@@ -35,10 +39,33 @@ export async function updateProfile(formData) {
 
 	// Revalidate cache
 	revalidatePath('/account/profile');
-	
+
 	return { success: true, message: 'Profile updated successfully' };
 }
+// Update the booking's details
 
+export async function deleteReservation(bookingId) {
+	const session = await auth();
+	if (!session) throw new Error('You must be signed in to update your profile');
+
+	const guestBookings = await getBookings(session.user.guestId);
+	const guestBookingsIds = guestBookings.map((booking) => booking.id);
+	if (!guestBookingsIds.includes(bookingId))
+		throw new Error('You are not authorized to delete this booking');
+
+	const { error } = await supabase
+		.from('bookings')
+		.delete()
+		.eq('id', bookingId);
+
+	if (error) throw new Error('Booking could not be deleted');
+	revalidatePath('/account/reservations');
+}
+
+// Update the booking's details
+export async updt
+
+// Sign in and sign out actions
 export async function signInAction() {
 	await signIn('google', { redirectTo: '/account' });
 }
