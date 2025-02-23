d254ba603786a9ca618069e52f722819dccd047a
Syedsafwan24 <136835578+Syedsafwan24@users.noreply.github.com>
2025-02-23 12:37:38 +0530

    final push:

--- app/_lib/action.js
+++ app/_lib/action.js
@@ -44,6 +44,32 @@ export async function updateProfile(formData) {
 	return { success: true, message: 'Profile updated successfully' };
 }
 
+export async function createBooking(bookingData, formData) {
+	const session = await auth();
+	if (!session)
+		throw new Error('You must be signed in to create a reservation');
+
+	const newBooking = {
+		...bookingData,
+		guestId: session.user.guestId,
+		numGuests: Number(formData.get('numGuests')),
+		observations: formData.get('observations').slice(0, 1000),
+		extrasPrice: 0,
+		totalPrice: bookingData.cabinPrice,
+		isPaid: false,
+		hasBreakfast: false,
+		status: 'unconfirmed',
+	};
+
+	const { error } = await supabase.from('bookings').insert([newBooking]);
+
+	if (error) {
+		throw new Error('Booking could not be created');
+	}
+	revalidatePath(`/cabins/${bookingData.cabinId}`);
+	redirect('/cabins/thankyou');
+}
+
 export async function deleteReservation(bookingId) {
 	const session = await auth();
 	if (!session) throw new Error('You must be signed in to update your profile');
